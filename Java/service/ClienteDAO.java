c213d4ddc28c3496916d0581c2adfc68285e348b
CarlosWindows <cadudailha68@gmail.com>
2025-11-21 16:18:06 -0300

    feat: Criar classe DAO de cliente

---
+++ Java/service/ClienteDAO.java
@@ -0,0 +1,72 @@
+package service;
+
+import model.Categoria;
+import model.Cliente;
+import model.ClientePF;
+import model.ClientePJ;
+import util.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class ClienteDAO {
+    private final String sqlInsert;
+
+    public ClienteDAO() {
+        sqlInsert = "INSERT INTO clientes(tipo, nome, telefone, categoria, documento) VALUES (?, ?, ?, ?, ?)";
+    }
+
+    public void insert(String tipo, String nome, String telefone, Categoria categoria,  String documento) {
+        try (Connection conn = Conexao.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sqlInsert)) {
+
+            Cliente cliente;
+            if (tipo.equals("PF")) {
+                pstmt.setString(1, tipo);
+
+                cliente = new ClientePF();
+                cliente.setNome(nome);
+                cliente.setTelefone(telefone);
+                cliente.setNivelFidelidade(categoria);
+                cliente.setDocumento(documento);
+            } else if (tipo.equals("PJ")) {
+                pstmt.setString(1, tipo);
+
+                cliente = new ClientePJ();
+                cliente.setNome(nome);
+                cliente.setTelefone(telefone);
+                cliente.setNivelFidelidade(categoria);
+                cliente.setDocumento(documento);
+            } else {
+                throw new IllegalArgumentException("O tipo deve ser literalmente \"PF\" ou \"PJ\" apenas");
+            }
+
+            pstmt.setString(2, cliente.getNome());
+            pstmt.setString(3, cliente.getTelefone());
+
+            switch (cliente.getNivelFidelidade()) {
+                case NULO -> {
+                    pstmt.setString(4, "Sem fidelidade");
+                }
+                case BRONZE -> {
+                    pstmt.setString(4, "Bronze");
+                }
+                case PRATA -> {
+                    pstmt.setString(4, "Prata");
+                }
+                case OUTRO -> {
+                    pstmt.setString(4, "Ouro");
+                }
+            }
+
+            pstmt.setString(5, documento);
+
+            pstmt.executeUpdate();
+        } catch (NullPointerException | IllegalArgumentException e) {
+            System.out.println("ERRO na integridade dos dados: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("ERRO (ClientesDAO/insert): " + e.getMessage());
+        }
+    }
+
+
+}
