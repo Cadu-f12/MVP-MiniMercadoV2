f8ff99fe8e02f6f97637e91cf779cfbdaceef302
CarlosLinux <cadudailha68@gmail.com>
2025-11-23 13:00:48 -0300

    feat: Criar método de cadastro de um produto no service

--- Java/service/ProdutoService.java
+++ Java/service/ProdutoService.java
@@ -1,4 +1,17 @@
 package service;
 
+import dao.ProdutoDAO;
+
 public class ProdutoService {
+
+    public void CadastrarProduto(String nome, String codBarras, double preco, int estoque) {
+        // Aqui deveria entrar um metodo para verificar se o atributo nome já existe no banco de dados.
+
+        ProdutoDAO produtoDAO = new ProdutoDAO();
+        try {
+            produtoDAO.insert(nome, codBarras, preco, estoque);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
