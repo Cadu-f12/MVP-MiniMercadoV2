cf85011791467fd77e7d46955edc8981f15f4d8c
CarlosLinux <cadudailha68@gmail.com>
2025-11-25 08:46:14 -0300

    feat: Criar atributo produtoDAO para a classe produtoService

--- Java/service/ProdutoService.java
+++ Java/service/ProdutoService.java
@@ -3,11 +3,12 @@ package service;
 import dao.ProdutoDAO;
 
 public class ProdutoService {
+    private final ProdutoDAO produtoDAO = new ProdutoDAO();
 
     public void CadastrarProduto(String nome, String codBarras, double preco, int estoque) {
         // Aqui deveria entrar um metodo para verificar se o atributo nome já existe no banco de dados.
 
-        ProdutoDAO produtoDAO = new ProdutoDAO();
+
         try {
             produtoDAO.insert(nome, codBarras, preco, estoque);
         } catch (Exception e) {
@@ -17,19 +18,19 @@ public class ProdutoService {
 
     public void ConsultarProduto(int id) {
 
-        ProdutoDAO produtoDAO = new ProdutoDAO();
+
         produtoDAO.select(id);
     }
 
     public void EditarProduto(int id, String nome, String codBarras, double preco, int estoque) {
 
-        ProdutoDAO produtoDAO = new ProdutoDAO();
+
         produtoDAO.update(id, nome, codBarras, preco, estoque);
     }
 
     public void DeletarProduto(int id) {
 
-        ProdutoDAO produtoDAO = new ProdutoDAO();
+
         produtoDAO.delete(id);
     }
 }
